caa0913c9acce214282b0ffb9f2a9b028a3940fb
Kimheejune <qwqw0102@hanmail.net>
2014-11-12 16:24:28 +0900

    sdfsdf

--- sketch.js
+++ sketch.js
@@ -2,9 +2,9 @@ var bg;
 var y = 0;
 
 function setup() {
-  // The background image must be the same size as the parameters
-  // into the size() method. In this program, the size of the image
-  // is 710x400 pixels.
+  
+  
+  
   bg = loadImage("space.png");
   img = loadImage("earth.png");
   img2 = loadImage("moon.png");
@@ -21,7 +21,7 @@ function draw() {
    fill(255,64,64);
    noStroke()
    image(img6, 140,140,270,270);
-   //노란
+   
    var duration = 4000;
    var timing = (new Date()%duration)/duration;
 
@@ -31,7 +31,7 @@ function draw() {
         250 + Math.sin(timing*2*PI)*200,       
         40,                                     
         40);                                    
-   //타원궤도 행성
+   
    var duration = 5000;
    var timing = (new Date()%duration)/duration;
 
@@ -41,35 +41,36 @@ function draw() {
         250 + Math.sin(timing*2*PI)*200,       
         150,                                     
         150);                                    
-   //속도 다른 행성
+   
    var duration = 8000;
    var timing = (new Date()%duration)/duration;
 
    fill(126,22,12);
    noStroke()
-   image(img5, 250 + Math.cos(timing*2*PI)*250,           // x좌표
-        250 + Math.sin(timing*2*PI)*200,       // y좌표
-        60,                                     // width
-        60);                                    // height
-   //태양를 도는 지구
+   image(img5, 250 + Math.cos(timing*2*PI)*250,           
+        250 + Math.sin(timing*2*PI)*200,       
+        60,                                     
+        60);                                    
+   
    var duration = 6000;
    var timing = (new Date()%duration)/duration;
 
    fill(81,158,81);
    noStroke()
-  image(img, 250 + Math.cos(timing*2*PI)*250,           // x좌표
-        250 + Math.sin(timing*2*PI)*200,       // y좌표
-        60,                                     // width
-        60);                                    // height
-   //지구를 도는 달
+  image(img, 250 + Math.cos(timing*2*PI)*250,          
+        250 + Math.sin(timing*2*PI)*200,       
+        60,                                     
+        60);                                   
+   
+
     var duration = 6000;
    var timing = (new Date()%duration)/duration;
 
    fill(209,192,70);
    noStroke()
-   image(img2,250 + Math.cos(timing*2*PI)*250+Math.cos(timing*4*PI)*80,          // x좌표
-        250 + Math.sin(timing*2*PI)*200+Math.sin(timing*4*PI)*80,       // y좌표
-        20,                                     // width
-        20);                                    // height
+   image(img2,250 + Math.cos(timing*2*PI)*250+Math.cos(timing*4*PI)*80,          
+        250 + Math.sin(timing*2*PI)*200+Math.sin(timing*4*PI)*80,       
+        20,                                    
+        20);                                   
 }
 
